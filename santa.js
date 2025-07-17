baad4a709e3b56275d6af171547c531ecb426793
TsukiGs <lunavegaromero@gmail.com>
2025-07-17 16:48:20 -0500

    santa.js

--- santa.js
+++ santa.js
@@ -1,3 +1,4 @@
+
 require('dotenv').config();
 
 const express = require('express');
@@ -361,7 +362,7 @@ client.on("message", (message) => {
                           .setDescription("Prefijo:  s!")
                           .setColor(0x00FFFF)
                           .addField("Comandos de diversion", "`say` `8ball`")
-                          .addField("Comandos de reaccion", "`hi`" `happy` `cry` `hug` `pat`)
+                          .addField("Comandos de reaccion", "`hi` `happy` `cry` `hug` `pat`")
                           .addField("Comandos de moderacion", "`clear` `ban` `kick` `redes`")
                           .addField("Otros", "`avatar` `userinfo` `invite` `help`")
                           .setAuthor("COMANDOS DEL BOT", client.user.avatarURL())
